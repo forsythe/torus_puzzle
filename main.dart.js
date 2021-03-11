33240cbdcd9579284006df316b9c01a24b70602a
forsythe <su.heng620@gmail.com>
2021-03-11 22:11:47 +0800

    add web-build

--- main.dart.js
+++ main.dart.js
@@ -21410,6 +21410,7 @@ CC:function(a,b){if(!$.a7G){$.a7G=!0
 window
 if(typeof console!="undefined")window.console.warn("The performance overlay isn't supported on the web")}},
 CD:function(a,b,c,d){var s,r
+c
 t.l9.a(b)
 s=b.a.b
 r=new H.ej(null)
@@ -56575,4 +56576,3 @@ a(b.target)}for(var r=0;r<s.length;++r)s[r].addEventListener("load",onLoad,false
 var s=F.a1s
 if(typeof dartMainRunner==="function")dartMainRunner(s,[])
 else s([])})})()
-//# sourceMappingURL=main.dart.js.map
